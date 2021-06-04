655e844d9bea8c54991ac3f998f71f4bb074c568
csgozoxing <84338349+csgozoxing@users.noreply.github.com>
2021-06-04 13:35:15 +0200

    Create generator.bat

---
+++ generator.bat
@@ -0,0 +1,17 @@
+@echo off
+color 3
+
+
+
+echo                                 _             
+echo                                | |            
+echo  __ _  ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+echo / _` |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+echo| (_| |  __/ | | |  __/ | | (_| | || (_) | |   
+echo \__, |\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+echo  __/ |                                        
+echo |___/                                         
+                                    
+
+echo Press space to generate psc! ; pause
+echo %random%%random%%random%%random%%random%-%random%%random%%random%%random%%random%-%random%%random%%random%%random%%random%-%random%%random%%random%%random%%random%
